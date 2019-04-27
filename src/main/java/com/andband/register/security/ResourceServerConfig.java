a7bb61f1fddd9ec10b95c52afc7d8517eb97d703
andband <finnhoulihan@email.com>
2019-04-27 20:05:06 +0100

    enabled web security

--- src/main/java/com/andband/register/security/ResourceServerConfig.java
+++ src/main/java/com/andband/register/security/ResourceServerConfig.java
@@ -2,11 +2,13 @@ package com.andband.register.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
 @EnableResourceServer
+@EnableWebSecurity
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
